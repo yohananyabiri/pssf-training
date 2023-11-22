05befaa13b83e29562ad19f97d4385f5f71910af
yohananyabiri <ynyabs@gmail.com>
2023-11-22 14:46:36 +0300

    3 commit

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 echo "Hello we have deployed our first application";
+echo "<br>";
 echo "Hello we have deployed our first application";
+echo "<br>";
+echo "Hello we have deployed second first application";
 ?>
